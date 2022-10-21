3390427fd8e297af8118efa8981215aae6d0a1ce
Daniel Kurtz <djkurtz@users.noreply.github.com>
2022-10-22 10:30:16 +1100

    Add physically correct lighting model

--- three/src/World/systems/renderer.js
+++ three/src/World/systems/renderer.js
@@ -3,6 +3,9 @@ import { WebGLRenderer } from '../../../vendor/three/build/three.module.js';
 function createRenderer() {
   const renderer = new WebGLRenderer();
 
+  // turn on the physically correct lighting model
+  renderer.physicallyCorrectLights = true;
+
   return renderer;
 }
 
