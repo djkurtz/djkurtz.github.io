f0fd75547f1d3637a3b537b4331a8808b8789962
Daniel Kurtz <djkurtz@users.noreply.github.com>
2022-10-22 16:49:05 +1100

    Add animation loop

--- three/src/World/World.js
+++ three/src/World/World.js
@@ -5,12 +5,14 @@ import { createScene } from './components/scene.js';
 
 import { createRenderer } from './systems/renderer.js';
 import { Resizer } from './systems/Resizer.js';
+import { Loop } from './systems/Loop.js';
 
 // These variables are module-scoped: we cannot access them
 // from outside the module
 let camera;
 let renderer;
 let scene;
+let loop;
 
 class World {
   // 1. Create an instance of the World app
@@ -18,25 +20,33 @@ class World {
     camera = createCamera();
     scene = createScene();
     renderer = createRenderer();
+    loop = new Loop(camera, scene, renderer);
     container.append(renderer.domElement);
     
-    const cube = createCube(2, 2, 2, 'orchid', 2, 0, 0);
-    const cube2 = createCube(1, 1, 1, 'blue', 0, 0, 1.5, 0, 0, 30);
+    const cube = createCube(2, 2, 2, 'chartreuse', 2, 0, 0);
+    const cube2 = createCube(1, 1, 1, 'pink', 0, 0, 1.5, 0, 0, 30);
     cube.add(cube2);
     const light = createLights();
+
+    loop.updatables.push(cube, cube2);
 
     scene.add(cube, light);
     
     const resizer = new Resizer(container, camera, renderer);
-    resizer.onResize = () => {
-      this.render();
-    };
   }
 
   // 2. Render the scene
   render() {
     // draw a single frame
     renderer.render(scene, camera);
+  }
+
+  start() {
+    loop.start();
+  }
+
+  stop() {
+    loop.stop();
   }
 }
 
--- three/src/World/components/cube.js
+++ three/src/World/components/cube.js
@@ -14,6 +14,16 @@ function createCube(w = 1, h = 1, d = 1, color = 'orchid', x = 0, y = 0, z = 0, 
   cube.position.set(x, y, z);
   cube.rotation.set(MathUtils.degToRad(rx), MathUtils.degToRad(ry), MathUtils.degToRad(rz));
 
+  const radiansPerSecond = MathUtils.degToRad(30);
+
+  // this method will be called once per frame
+  cube.tick = (delta) => {
+    // increase the cube's rotation each frame
+    cube.rotation.z += radiansPerSecond * delta;
+    cube.rotation.x += radiansPerSecond * delta;
+    cube.rotation.y += radiansPerSecond * delta;
+  };
+
   return cube;
 }
 
