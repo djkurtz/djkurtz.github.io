a21364a1f3669b0716b8955a7d86896d3a51a369
Daniel Kurtz <djkurtz@users.noreply.github.com>
2022-10-23 09:50:20 +1100

    Textured cubes

--- three/src/World/components/cube.js
+++ three/src/World/components/cube.js
@@ -1,12 +1,42 @@
-import { BoxGeometry, MathUtils, Mesh, MeshStandardMaterial } from '../../../vendor/three/build/three.module.js';
+import {
+  BoxGeometry, CapsuleGeometry, CircleGeometry, ConeGeometry, CylinderGeometry, DodecahedronGeometry,
+  EdgesGeometry, ExtrudeGeometry, IcosahedronGeometry, LatheGeometry, OctahedronGeometry, PlaneGeometry, PolyhedronGeometry, RingGeometry,
+  ShapeGeometry, SphereGeometry, TetrahedronGeometry, TorusGeometry, TorusKnotGeometry, TubeGeometry, WireframeGeometry,
+  MathUtils,
+  Mesh,
+  MeshStandardMaterial,
+  TextureLoader,
+ } from '../../../vendor/three/build/three.module.js';
+
+function createMaterial(color) {
+  // create a texture loader.
+  const textureLoader = new TextureLoader();
+
+  // load a texture
+  const texture_col = textureLoader.load(
+    '../../../assets/textures/uv-test-col.png'
+  );
+  const texture_bw = textureLoader.load(
+    '../../../assets/textures/uv-test-bw.png',
+  );
+
+  // create a "standard" material
+  // the texture we just loaded as a color map
+  const material = new MeshStandardMaterial({
+    alphaMap: texture_col,
+    map: texture_bw,
+    transparent: true,
+    color: color
+  });
+
+  return material;
+}
 
 function createCube(w = 1, h = 1, d = 1, color = 'orchid', x = 0, y = 0, z = 0, rx = -45, ry = -20, rz = 30) {
   // create a geometry
-  const geometry = new BoxGeometry(w, h, d);
+  const geometry = new BoxGeometry();//w , h, d);
 
-  // Switch the old "basic" material to
-  // a physically correct "standard" material
-  const material = new MeshStandardMaterial({ color: color });
+  const material = createMaterial(color);
 
   // create a Mesh containing the geometry and material
   const cube = new Mesh(geometry, material);
@@ -19,9 +49,9 @@ function createCube(w = 1, h = 1, d = 1, color = 'orchid', x = 0, y = 0, z = 0, 
   // this method will be called once per frame
   cube.tick = (delta) => {
     // increase the cube's rotation each frame
-    cube.rotation.z += radiansPerSecond * delta;
-    cube.rotation.x += radiansPerSecond * delta;
-    cube.rotation.y += radiansPerSecond * delta;
+    cube.rotation.z += delta * radiansPerSecond;
+    cube.rotation.x += delta * radiansPerSecond;
+    cube.rotation.y += delta * radiansPerSecond;
   };
 
   return cube;
