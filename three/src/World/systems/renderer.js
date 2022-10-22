31167c1377dd01b53880c347b127ecd4348f820b
Daniel Kurtz <djkurtz@users.noreply.github.com>
2022-10-22 15:34:36 +1100

    Enable antialias

--- three/src/World/systems/renderer.js
+++ three/src/World/systems/renderer.js
@@ -1,7 +1,7 @@
 import { WebGLRenderer } from '../../../vendor/three/build/three.module.js';
 
 function createRenderer() {
-  const renderer = new WebGLRenderer();
+  const renderer = new WebGLRenderer({ antialias: true });
 
   // turn on the physically correct lighting model
   renderer.physicallyCorrectLights = true;
