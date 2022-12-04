d6fdad962c19c8863aa296b2a9455e0b25704e76
Daniel Kurtz <djkurtz@users.noreply.github.com>
2022-12-05 10:39:03 +1100

    menu interval pause

--- life/game.js
+++ life/game.js
@@ -9,9 +9,14 @@ const config = {
   dead_color: 0x000000,
   live_color: 0xffffff,
   border: {
+    menu: 20,
     min: 10,
   },
-  period: 60,
+  interval: {
+    range: [ 0, 15, 25, 50, 100, 250, 500, 1000, 2000 ],
+    start_idx: 4,
+  },
+  start_live_chance: 0.5,
 };
 
 class Game extends Phaser.Scene
@@ -20,28 +25,47 @@ class Game extends Phaser.Scene
     super('game');
 
     this.cells;
+    this.prev_tick = 0;
     this.ticks = 0;
+    this.interval_idx = config.interval.start_idx;
+    this.population = 0;
     this.border_spacing = config.cell.size + config.cell.border.thickness;
+
+    this.run = true;
   }
 
   create () {
+    this.tick_text = this.add.text(10, 10, 'Ticks: ' + this.ticks,
+        { font: '14px Monospace', fill: '#ffffff' });
+    this.interval_text = this.add.text(210, 10, 'Interval: ' + this.interval_get() + ' ms',
+        { font: '14px Monospace', fill: '#ffffff' });
+    this.population_text = this.add.text(410, 10, 'Population: 0',
+        { font: '14px Monospace', fill: '#ffffff' });
+    this.pause_text = this.add.text(610, 10, 'PAUSED',
+        { font: '14px Monospace', fill: '#880000' }).setVisible(false);
+
     const pw = this.cameras.main.width - 2 * config.border.min;
-    const ph = this.cameras.main.height - 2 * config.border.min;
+    const ph = this.cameras.main.height - 2 * config.border.min - config.border.menu;
 
     this.Nx = Math.floor((pw - config.cell.border.thickness) / this.border_spacing);
     this.Ny = Math.floor((ph - config.cell.border.thickness) / this.border_spacing);
 
     this.Sx = Math.floor((this.cameras.main.width - this.Nx * this.border_spacing) / 2);
-    this.Sy = Math.floor((this.cameras.main.height - this.Ny * this.border_spacing) / 2);
+    this.Sy = Math.floor((this.cameras.main.height + config.border.menu - this.Ny * this.border_spacing) / 2);
+
+    this.input.keyboard.on('keydown-UP', function(event) { this.interval_dec(); }, this);
+    this.input.keyboard.on('keydown-DOWN', function(event) { this.interval_inc(); }, this);
+
+    this.input.keyboard.on('keydown-S', function(event) { this.stop_start(); }, this);
 
     this.graphics = this.add.graphics();
 
     // Initialize cells
     this.cells = new Array(this.Ny);
     for (let y = 0; y < this.Ny; y++) {
-      this.cells[y] = new Array(this.Nx).fill(0);
+      this.cells[y] = new Array(this.Nx);
       for (let x = 0; x < this.Nx; x++) {
-        this.cells[y][x] = Math.round(Math.random());
+        this.cells[y][x] = (Math.random() <= config.start_live_chance) ? 1 : 0;
       }
     }
 
@@ -55,9 +79,6 @@ class Game extends Phaser.Scene
         this.drawCell(x, y, this.cells[y][x]);
       }
     }
-
-    // Add a new asteroid every 1 sec
-    this.timedEvent = this.time.addEvent({ delay: 120, callback: this.updateCells, callbackScope: this, loop: true });
   }
 
   drawCell(x, y, cell) {
@@ -70,7 +91,41 @@ class Game extends Phaser.Scene
     this.graphics.fillRect(dx, dy, config.cell.size, config.cell.size);
   }
 
-  updateCells() {
+  stop_start () {
+    this.run = !this.run;
+    this.pause_text.setVisible(!this.run);
+  }
+
+  interval_get ( ) {
+    return config.interval.range[this.interval_idx];
+  }
+
+  interval_set (idx) {
+    this.interval_idx = Math.max(0, Math.min(idx, config.interval.range.length - 1));
+    this.interval = config.interval.range[this.interval_idx];
+    this.interval_text.setText('Interval: ' + this.interval + ' ms');
+  }
+
+  interval_inc () {
+    this.interval_set(this.interval_idx + 1);
+  }
+
+  interval_dec () {
+    this.interval_set(this.interval_idx - 1);
+  }
+
+  update ( time, delta ) {
+    if (!this.run)
+      return;
+
+    if (time - this.prev_tick < this.interval_get())
+      return;
+    
+    this.prev_tick = time;
+
+    this.ticks += 1;
+    this.tick_text.setText('Ticks: ' + this.ticks);
+
     let next = new Array(this.Ny);
     for (let y = 0; y < this.Ny; y++) {
       next[y] = new Array(this.Nx);
@@ -82,11 +137,15 @@ class Game extends Phaser.Scene
       }
     }
 
+    let population = 0;
     for (let y = 0; y < this.Ny; y++) {
       for (let x = 0; x < this.Nx; x++) {
         this.cells[y][x] = next[y][x];
+        population += next[y][x];
       }
     }
+    this.population = population;
+    this.population_text.setText('Population: ' + this.population);
   }
 
   count_neighbors(x, y, cells) {
